75ad259c81bad450a7da18407bb469d0270f3508
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-05-17 23:50:06 -0500

    added a function to calculate teaching hours per semester

--- scripts/curriculumlib.py
+++ scripts/curriculumlib.py
@@ -1,9 +1,12 @@
 import xml.etree.ElementTree as ET
+from os import walk
+from collections import defaultdict
 
 class Syllabus:
   title = None
   subject = None
   number = None
+  offered = None
   
 def parse_syllabus(xmlfile):
   ns = '{https://csmp.missouriwestern.edu}'
@@ -13,5 +16,32 @@ def parse_syllabus(xmlfile):
   syllabus.title = dt.find(ns+'title').text
   syllabus.subject = dt.find(ns+'subject').text
   syllabus.number = dt.find(ns+'number').text
+  syllabus.offered = list()
+  for semestert in dt.findall(ns+'offered'):
+    semester = semestert.text
+    if semester == 'spring':
+      syllabus.offered.append('spring-even')
+      syllabus.offered.append('spring-odd')
+    elif semester == 'fall':
+      syllabus.offered.append('fall-even')
+      syllabus.offered.append('fall-odd')
+    else:
+      syllabus.offered.append(semester)
+
   return syllabus
-  
+
+def load_syllabi():
+  return [parse_syllabus(open('syllabi/'+filename)) for filename in next(walk('syllabi'))[2]]
+
+
+def hours_per_semester():
+  # update to use credit hours instead of # of courses
+  semesters = defaultdict(list)
+  for course in load_syllabi():
+    for semester in course.offered:
+      semesters[semester].append(course)
+  ret = []
+  for semester in semesters:
+    ret.append((semester, len(semesters[semester])))
+  return ret
+
