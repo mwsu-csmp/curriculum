2a1408202254b848bbda67f963a3b72721d267f9
crandles55 <crandles@missouriwestern.edu>
2019-06-19 13:00:32 -0500

    added schema for standards

--- scripts/validate-everything.sh
+++ scripts/validate-everything.sh
@@ -23,7 +23,16 @@ done
 
 for f in syllabi/*
 do
-	xmllint --schema schema/* --noout $f
+	xmllint --schema schema/course.xsd --noout $f
+	if [ $? -ne 0 ]; then
+	    	echo "*** $f failed to validate"
+		exit
+	fi
+done  
+
+for f in standards/*.xml
+do
+	xmllint --schema schema/standard.xsd --noout $f
 	if [ $? -ne 0 ]; then
 	    	echo "*** $f failed to validate"
 		exit
