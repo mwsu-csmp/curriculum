70b6a856fb9460b5b190f067d8719f8a7608e07e
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-07-15 12:14:17 -0700

    added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mwsu_curriculum',
+      version='0.1',
+      description='Curriculum definitions and utilities for MWSU Computer Science',
+      url='https://github.com/mwsu-csmp/curriculum',
+      author='MWSU Computer Science Department',
+      author_email='csmp@missouriwestern.edu',
+      license='GPL',
+      packages=['mwsu_curriculum'],
+      zip_safe=False)
