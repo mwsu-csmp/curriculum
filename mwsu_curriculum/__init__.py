b610e244cf0def965b93c2edbc4c712a184e374c
crandles55 <crandles@missouriwestern.edu>
2019-08-27 19:46:21 -0500

    added a funtion to curriculumlib that adds all the data to models

--- mwsu_curriculum/__init__.py
+++ mwsu_curriculum/__init__.py
@@ -1,2 +1,4 @@
 from .curriculumlib import load_syllabi
 from .curriculumlib import hours_per_semester
+from .curriculumlib import courses_per_semester
+from .curriculumlib import parse_course
--- mwsu_curriculum/curriculumlib.py
+++ mwsu_curriculum/curriculumlib.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import os
 from os import walk
 from collections import defaultdict
 from pkg_resources import resource_filename
@@ -8,7 +9,12 @@ class Syllabus:
   subject = None
   number = None
   offered = None
-  workload_hours = None
+  workload_hours = 0
+  scheduleType = None
+  catalogDescription = None
+  prerequisites = None
+  objective = None
+  topic = None
   
 def parse_syllabus(xmlfile):
   ns = '{https://csmp.missouriwestern.edu}'
@@ -39,6 +45,37 @@ def load_syllabi():
     syllabi.append(parse_syllabus(open(resource_filename('mwsu_curriculum', 'syllabi/'+filename))))
   return syllabi
 
+def parse_course(filename):
+  ns = '{https://csmp.missouriwestern.edu}'
+  syllabus = Syllabus()
+  dt = ET.parse(filename).getroot()
+  syllabus.title = dt.find(ns + 'title').text
+  syllabus.subject = dt.find(ns + 'subject').text
+  syllabus.number = dt.find(ns + 'number').text
+  syllabus.workload_hours = int(3 if not 'workloadHoursLecture' in dt.attrib else dt.attrib['workloadHoursLecture'])
+  syllabus.offered = list()
+  for semestert in dt.findall(ns + 'offered'):
+    semester = semestert.text
+    syllabus.offered.append(semester)
+  syllabus.scheduleType = dt.find(ns + 'scheduleType').text
+  syllabus.catalogDescription = dt.find(ns + 'catalogDescription').text
+  syllabus.prerequisites = dt.find(ns + 'prerequisites').text
+  syllabus.objective = list()
+  for objectivet in dt.findall(ns + 'objective'):
+    objective = objectivet.text
+    syllabus.objective.append(objective)
+  syllabus.topic = list()
+  for topict in dt.findall(ns + 'topic'):
+    topic = topict.text
+    syllabus.topic.append(topic)
+
+  return syllabus
+
+def load_courses():
+  syllabi = []
+  for filename in next(walk(resource_filename('mwsu_curriculum', 'syllabi')))[2]:
+    syllabi.append(parse_course(open(resource_filename('mwsu_curriculum', 'syllabi/'+filename))))
+  return syllabi
 
 def hours_per_semester():
   # update to use credit hours instead of # of courses
@@ -51,3 +88,9 @@ def hours_per_semester():
     ret.append((semester, sum(map((lambda course: course.workload_hours), semesters[semester]))))
   return ret
 
+def courses_per_semester():
+  # update to use credit hours instead of # of courses
+  syllabus = Syllabus
+  semesters = []
+  for course in load_syllabi():
+    return syllabus
