044f18691eaaae92055cd1a5c79f2410712b18b2
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2020-03-27 15:30:17 -0500

    exposed load_program function

--- mwsu_curriculum/__init__.py
+++ mwsu_curriculum/__init__.py
@@ -6,6 +6,7 @@ from .curriculumlib import load_standard
 from .curriculumlib import load_standards
 from .curriculumlib import load_syllabus
 from .curriculumlib import load_syllabi
+from .curriculumlib import load_program
 from .curriculumlib import load_roster
 from .curriculumlib import load_schedule
 from .curriculumlib import available_years
